e3f6ab71ceddac1da3cb61f90db99b7bd79d6dfe
EdwanMarks <99619218+EdwanMarks@users.noreply.github.com>
2024-05-15 20:26:50 -0300

    Adicionar Produto

---
+++ js/revolution/js/adicionar.js
@@ -0,0 +1,16 @@
+document.getElementById('addProductForm').addEventListener('submit', async (event) => {
+            event.preventDefault();
+            const nome = document.getElementById('nome').value;
+            const descricao = document.getElementById('descricao').value;
+            const codigo = document.getElementById('codigo').value;
+            const cor = document.getElementById('cor').value;
+            const response = await fetch('/api/produtos', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ nome, descricao, codigo, cor }),
+            });
+            const result = await response.json();
+            alert(result.mensagem);
+});
