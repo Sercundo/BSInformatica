46a86d8a62e1187425c659af412c0a731255348c
EdwanMarks <99619218+EdwanMarks@users.noreply.github.com>
2024-05-15 20:32:13 -0300

    Atualizar Produtos

---
+++ js/revolution/js/atualizar.js
@@ -0,0 +1,20 @@
+document.getElementById('updateProductForm').addEventListener('submit', async (event) => {
+            event.preventDefault();
+            const id = document.getElementById('id').value;
+            const nome = document.getElementById('nome').value;
+            const descricao = document.getElementById('descricao').value;
+            const codigo = document.getElementById('codigo').value;
+            const cor = document.getElementById('cor').value;
+            const response = await fetch(`/api/produtos/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ nome, descricao, codigo, cor }),
+            });
+            if (response.ok) {
+                alert('Produto atualizado com sucesso.');
+            } else {
+                alert('Erro ao atualizar o produto.');
+            }
+});
